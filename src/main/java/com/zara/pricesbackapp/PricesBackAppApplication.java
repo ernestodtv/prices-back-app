934d4796fb608814218347bf77cb224f86a8f856
e_edetov <e_edetov@mercadona.com>
2022-10-31 18:56:20 +0100

    Initial commit

---
+++ src/main/java/com/zara/pricesbackapp/PricesBackAppApplication.java
@@ -0,0 +1,13 @@
+package com.zara.pricesbackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricesBackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricesBackAppApplication.class, args);
+	}
+
+}
